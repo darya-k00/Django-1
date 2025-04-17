59cae719c7e4cfbb14f320c5be74b54f75136ff1
darya-k00 <dasha.kon.060203@gmail.com>
2025-04-17 09:52:08 +0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -1,11 +1,16 @@
+import os 
+
+from dotenv import load_dotenv
+load_dotenv()
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': '',
-        'PORT': '',
-        'NAME': '',
-        'USER': '',
-        'PASSWORD': '',
+        'HOST': os.environ("HOST_DB"),
+        'PORT': os.environ("PORT_DB"),
+        'NAME': os.environ("NAME_DB"),
+        'USER': os.environ("USER_DB"),
+        'PASSWORD': os.environ("PASSWORD_DB"),
     }
 }
 
