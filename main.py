29bf1554cf4d13a064d539632d43629ab948cbc8
darya-k00 <dasha.kon.060203@gmail.com>
2025-04-17 09:51:40 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import os
-
+from dotenv import load_dotenv
+load_dotenv()
 import django
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
 
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard 
 
 if __name__ == '__main__':
-    # Программируем здесь
-    print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    passcards = Passcard.objects.all()
+    for passcard in passcards:
+        active_passcards = Passcard.objects.filter(is_active=True)  
+    print(f"Всего пропусков: {Passcard.objects.count()}")
+    print(f"Активных пропусков: {len(active_passcards)}")
